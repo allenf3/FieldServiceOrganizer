3e740079f1b48cb2991e738c26391de3061bfcd3
Fred Allen <allenf3@msn.com>
2021-03-12 22:09:15 -0600

    Made location public and added LocationsOverview page

---
+++ Client/Pages/LocationsOverview.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceOrganizer.Client.Pages
+{
+    public class LocationsOverview
+    {
+    }
+}
