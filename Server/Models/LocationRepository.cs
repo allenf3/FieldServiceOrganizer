83e555505ee2f6399d32ed15523b91e3d7438884
Fred Allen <allenf3@msn.com>
2021-03-18 01:37:55 -0500

    Added ILocationRepository and LocationRepository

---
+++ Server/Models/LocationRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceOrganizer.Server.Models
+{
+    public class LocationRepository
+    {
+    }
+}
