67739a0ba6b12613a0ecf27dfb2c2254f8a46a3c
Fred Allen <allenf3@msn.com>
2021-04-03 22:09:59 -0500

    Removed location test data

--- FieldServiceOrganizer.Server/Pages/LocationList.cs
+++ FieldServiceOrganizer.Server/Pages/LocationList.cs
@@ -43,73 +43,15 @@ namespace FieldServiceOrganizer.Server.Pages
             {
                 Locations.Add(item);
             }
+
+            newLocation.OccupantName = string.Empty;
+            newLocation.FullAddress = string.Empty;
+            newLocation.City = string.Empty;
+            newLocation.State = string.Empty;
+            newLocation.Zip = string.Empty;
+
             return Locations;
-
-
-
-            //await foreach (var item in cosmosItems)
-            //{
-            //    locations.Add(item);
-            //}
-            //return locations;
         }
-            //Location location1 = new Location
-            //{
-            //    Id = "1",
-            //    OccupantName = "Denny's",
-            //    Direction = "N",
-            //    FullAddress = "123 N. South Street",
-            //    City = "Anytown",
-            //    State = "MO",
-            //    Zip = "55455"
-            //};
-            //Location location2 = new Location
-            //{
-            //    Id = "2",
-            //    OccupantName = "Clementines",
-            //    FullAddress = "555 55Th Ave.",
-            //    City = "Mooresville",
-            //    State = "IL",
-            //    Zip = "84384"
-            //};
-            
-            //Location location3 = new Location
-            //{
-            //    Id = "3",
-            //    OccupantName = "Master Auto Repair",
-            //    FullAddress = "928 Beasley St.",
-            //    City = "Greyville",
-            //    State = "NC",
-            //    Zip = "55455-4882"
-            //};
-            
-            //Location location4 = new Location
-            //{
-            //    Id = "4",
-            //    OccupantName = "Warby Parker",
-            //    FullAddress = "4886 Highway 17",
-            //    City = "Twosville",
-            //    State = "IL",
-            //    Zip = "88343"
-            //};
-            
-            //Location location5 = new Location
-            //{
-            //    Id = "5",
-            //    OccupantName = "Mattress City",
-            //    FullAddress = "9932 Oak Blvd.",
-            //    City = "Yorkshire",
-            //    State = "TN",
-            //    Zip = "85874"
-            //};
-
-            //Locations = new List<Location> { location1, location2, location3, location4, location5 };
-
-            //newLocation.OccupantName = string.Empty;
-            //newLocation.FullAddress = string.Empty;
-            //newLocation.City = string.Empty;
-            //newLocation.State = string.Empty;
-            //newLocation.Zip = string.Empty;
         
     }
 }
