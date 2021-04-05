9ffdfb196ccd171cd58af6d0e572355a051d4ace
Fred Allen <allenf3@msn.com>
2021-04-04 20:45:43 -0500

    Created MelissaApi interface and service and added new Melissa class

---
+++ FieldServiceOrganizer.Server/Services/IMelissaApiService.cs
@@ -0,0 +1,13 @@
+﻿using FieldServiceOrganizer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceOrganizer.Server.Services
+{
+    public interface IMelissaApiService
+    {
+        Task<MelissaNormalizedLocation> GetNormalizedLocation(Location location);
+    }
+}
---
+++ FieldServiceOrganizer.Server/Services/MelissaApiService.cs
@@ -0,0 +1,16 @@
+﻿using FieldServiceOrganizer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceOrganizer.Server.Services
+{
+    public class MelissaApiService : IMelissaApiService
+    {
+        public Task<MelissaNormalizedLocation> GetNormalizedLocation(Location location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
