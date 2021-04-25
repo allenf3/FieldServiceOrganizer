db83c4682f5d8a8a0b296eb3fb982c605320e78f
Fred Allen <allenf3@msn.com>
2021-04-25 01:24:15 -0500

    Beginning work on Bing maps route API

---
+++ FieldServiceOrganizer.Server/Services/IBingMapsApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FieldServiceOrganizer.Models;
+
+namespace FieldServiceOrganizer.Server.Services
+{
+    public interface IBingMapsApiService
+    {
+        Task<BingMapsResponse> GetPointToPointRoute(Location From, Location To);
+    }
+}
